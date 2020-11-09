dfac1f903efc83888a8f882477584f69c31e7a01
PierminatorQ <pierrequintero@gmail.com>
2020-11-08 23:45:27 -0300

    images added to product - minimagick added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:title, :description, :author, :initial_price, :delivery_cost, :user_id, :category_id)
+      params.require(:product).permit(:title, :description, :author, :initial_price, :delivery_cost, :user_id, :image, :category_id)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :auctions
+  has_one_attached :image
 end
