35f6c94867d1f423b87f68072d2bd95f60762c96
PierminatorQ <pierrequintero@gmail.com>
2020-11-16 12:39:32 -0300

    home style added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     
     if( params[:search] && !params[:search].empty? )
-      @auctions = Auction.where("title LIKE ?", "%#{params[:search]}%").order(created_at: :desc)  
+      @auctions = Auction.where("title LIKE ?", "%#{params[:search]}%").only_active(@auction).order(created_at: :desc)  
     else
       @auctions = Auction.only_active(@auction).order(created_at: :desc)
     end
